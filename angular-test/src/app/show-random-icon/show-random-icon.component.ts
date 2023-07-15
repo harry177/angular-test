e709b7e93833e0668cc4f7b315afd3ef77d16c65
Harry177 <harry177@rambler.ru>
2023-07-15 15:55:22 +0200

    fix: correct mistakes related to normal work of the component

--- angular-test/src/app/show-random-icon/show-random-icon.component.ts
+++ angular-test/src/app/show-random-icon/show-random-icon.component.ts
@@ -1,53 +1,30 @@
 import { Component } from '@angular/core';
-import { IconName, IconPrefix, findIconDefinition } from '@fortawesome/fontawesome-svg-core';
-import { IconDefinition, faIcons } from '@fortawesome/free-solid-svg-icons';
-
-interface CustomIconDefinition extends IconDefinition {
-  prefix: IconPrefix;
-  iconName: IconName;
-  icon: [number, number, string[], string, string | string[]];
-}
-
+import { IconPack, IconProp } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-show-random-icon',
   templateUrl: './show-random-icon.component.html',
   styleUrls: ['./show-random-icon.component.scss'],
 })
 export class ShowRandomIconComponent {
-  faIcons: Record<string, CustomIconDefinition> = {};
-  showIcon = false;
-  randomIcon: CustomIconDefinition | undefined;
+  icons: IconPack = fas;
+  showIcon: boolean = false;
+  randomIcon: IconProp;
 
   constructor() {
-    const iconNames = Object.keys(faIcons);
-    for (const iconName of iconNames) {
-      const definition = findIconDefinition({
-        prefix: 'fas' as IconPrefix,
-        iconName: iconName as IconName,
-      });
-      if (definition) {
-        const [width, height, ligatures, unicode, svgPathData] = definition.icon;
-        this.faIcons[iconName] = {
-          prefix: 'fas',
-          iconName: iconName as IconName,
-          icon: [width, height, ligatures, unicode, svgPathData],
-        };
-      }
-    }
-
-    const iconValues = Object.values(this.faIcons);
-    const randomIndex = Math.floor(Math.random() * iconValues.length);
-    this.randomIcon = iconValues[randomIndex];
+    const iconKeys = Object.keys(this.icons);
+    const randomIndex = Math.floor(Math.random() * iconKeys.length);
+    this.randomIcon = this.icons[randomIndex];
   }
 
   showRandomIcon() {
     this.showIcon = false;
     setTimeout(() => {
-      const iconValues = Object.values(this.faIcons);
-      const randomIndex = Math.floor(Math.random() * iconValues.length);
-      this.randomIcon = iconValues[randomIndex];
+      const iconKeys = Object.keys(this.icons);
+      const randomIndex = Math.floor(Math.random() * iconKeys.length);
+      this.randomIcon = this.icons[iconKeys[randomIndex]];
       this.showIcon = true;
-      console.log(this.randomIcon)
+      console.log(this.icons[iconKeys[randomIndex]]);
     }, 3000);
   }
 }
